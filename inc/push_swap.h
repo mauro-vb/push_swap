2022c38333a3ebd4225db8ff0201c40c7b541f66
Mikhail Peskov <mpeskov@cbr15s4.42barcelona.com>
2026-01-15 13:17:08 +0100

    bubble_sort v0.9

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mvazquez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 12:29:08 by mvazquez          #+#    #+#             */
-/*   Updated: 2026/01/13 16:19:20 by mvazquez         ###   ########.fr       */
+/*   Updated: 2026/01/15 13:16:26 by mpeskov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ void	stackadd_front(t_stack **stack, t_stack *new);
 void	stackadd_back(t_stack **stack, t_stack *new);
 size_t	stacksize(t_stack *head);
 void	index_stack(t_stack **stack);
+int		is_sorted(t_stack *stack)
 
 float	compute_disorder(t_stack *stack);
 
@@ -49,4 +50,8 @@ int	rr(t_stack **stack_a, t_stack **stack_b);
 int	rra(t_stack **stack_a);
 int	rrb(t_stack **stack_b);
 int	rrr(t_stack **stack_a, t_stack **stack_b);
+
+//simple sort
+
+void	bubble_sort(t_stack	**stack);
 #endif
