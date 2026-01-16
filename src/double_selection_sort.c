0c532d7ebf9b331466be195828ddef63ebae2ec8
Mauro Vazquez Bassat <mvazquez@cbr15s2.42barcelona.com>
2026-01-16 14:31:16 +0100

    started turk

--- src/double_selection_sort.c
+++ src/double_selection_sort.c
@@ -6,7 +6,7 @@
 /*   By: mvazquez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 14:27:39 by mvazquez          #+#    #+#             */
-/*   Updated: 2026/01/15 17:51:12 by mvazquez         ###   ########.fr       */
+/*   Updated: 2026/01/16 13:00:52 by mvazquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,108 +14,35 @@
 
 void	sort_three(t_stack **stack, int is_a)
 {
-	int head_val;
-	int	tail_val;
-	int	mid_val;
+	int		head_val;
+	int		tail_val;
+	int		mid_val;
+	t_ops	ops;
 
+	if (is_a)
+		ops = (t_ops){sa, ra, rra};
+	else
+		ops = (t_ops){sb, rb, rrb};
 	if (stacksize(*stack) != 3 || is_sorted(*stack))
 		return ;
 	head_val = (*stack)->index;
 	tail_val = (*stack)->previous->index;
 	mid_val = (*stack)->next->index;
-	if (tail_val > head_val&& tail_val > mid_val && head_val > mid_val)
-	{
-		if (is_a)
-			sa(stack);
-		else
-			sb(stack);
-	}
+	if (tail_val > head_val && tail_val > mid_val && head_val > mid_val)
+		ops.swp(stack);
 	else if (head_val > tail_val && head_val > mid_val && mid_val < tail_val)
-	{
-		if (is_a)
-			ra(stack);
-		else
-			rb(stack);	
-	}
+		ops.rot(stack);
 	else if (head_val > tail_val && head_val < mid_val && mid_val > tail_val)
-	{
-		if (is_a)
-			rra(stack);
-		else
-			rrb(stack);	
-	}
+		ops.rrot(stack);
 	else if (head_val > tail_val && head_val > mid_val && mid_val > tail_val)
 	{
-		if (is_a)
-		{
-			sa(stack);
-			rra(stack);
-		}
-		else
-		{
-			sb(stack);
-			rrb(stack);
-		}
+		ops.swp(stack);
+		ops.rrot(stack);
 	}
 	else if (head_val < tail_val && head_val < mid_val && mid_val > tail_val)
 	{
-		if (is_a)
-		{
-			sa(stack);
-			ra(stack);
-		}
-		else
-		{
-			sb(stack);
-			rb(stack);
-		}
+		ops.swp(stack);
+		ops.rot(stack);
 	}
-		
 }
 
-void	double_selection_sort(t_stack **stack_a, t_stack **stack_b)
-{
-	int	min_i;
-	int	max_i;
-	int	max_index;
-
-	if (!stack_a || !*stack_a || (*stack_a)->next == *stack_a)
-		return ;
-	max_index = stacksize(*stack_a) - 1;
-	min_i = 0;
-	max_i = max_index;
-	while (*stack_a)
-	{
-		print_stack(*stack_a, 1);
-		ft_printf("min: %i, max: %i\n", min_i, max_i);
-		if ((*stack_a)->index == min_i || (*stack_a)->index == max_i)
-		{
-			if ((*stack_a)->index == min_i)
-			{
-				if (stacksize(*stack_b) > 1 && min_i != 0)
-				{
-					while ((*stack_b)->index != min_i - 1)
-						rb(stack_b);
-				}
-				pb(stack_a, stack_b);
-				min_i++;
-			}
-			else if ((*stack_a)->index == max_i)
-			{
-				if (stacksize(*stack_b) > 1 && max_i != max_index)
-				{
-					while ((*stack_b)->index != max_i + 1)
-						rb(stack_b);
-				}
-				pb(stack_a, stack_b);
-				max_i--;
-			}
-		}
-		else
-			ra(stack_a);
-	}
-	while ((*stack_b)->index != 0)
-		rb(stack_b);
-	while(*stack_b)
-		pa(stack_a, stack_b);
-}
